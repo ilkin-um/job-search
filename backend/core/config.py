835782df90a22dfcdc3e3cffc54e4faee40e9a15
ilkin-um <ilkinum@gmail.com>
2022-06-17 17:24:49 +0400

    connected db to the application

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,15 @@
+from decouple import config
+
+
 class Settings:
+
     PROJECT_NAME: str = "Job Search"
     PROJECT_VERSION: str = "0.1.0"
+    DB_NAME = config("DB_NAME")
+    DB_USER = config("DB_USER")
+    DB_HOST = config("DB_HOST")
+    DB_PORT = config("DB_PORT")
+    DB_PASSWORD = config("DB_PASSWORD")
 
 
 settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI
 from core.config import settings
 from apis.general.route_home import home_router
+from fastapi.staticfiles import StaticFiles
+from db.session import engine  # new
+from db.base import Base  # new
 
 
 def include_router(app):
     app.include_router(home_router)
 
 
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
+    configure_static(app)
+    create_tables()
     return app
 
 
