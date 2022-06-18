3a1c914d8c8983b525d9869458ee0a1d23780452
ilkin-um <ilkinum@gmail.com>
2022-06-18 11:55:22 +0400

    created db models

--- backend/core/config.py
+++ backend/core/config.py
@@ -5,11 +5,11 @@ class Settings:
 
     PROJECT_NAME: str = "Job Search"
     PROJECT_VERSION: str = "0.1.0"
-    DB_NAME = config("DB_NAME")
-    DB_USER = config("DB_USER")
-    DB_HOST = config("DB_HOST")
-    DB_PORT = config("DB_PORT")
-    DB_PASSWORD = config("DB_PASSWORD")
+    DB_NAME: str = config("DB_NAME")
+    DB_USER: str = config("DB_USER")
+    DB_HOST: str = config("DB_HOST")
+    DB_PORT: str = config("DB_PORT")
+    DB_PASSWORD: str = config("DB_PASSWORD")
 
 
 settings = Settings()
--- backend/db/base.py
+++ backend/db/base.py
@@ -1,12 +1,3 @@
-from typing import Any
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
-
-
-@as_declarative()
-class Base:
-    id: Any
-    __name__: str
-
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+from db.base_model import Base
+from db.models.jobs import Job
+from db.models.users import User
