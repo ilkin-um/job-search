5416ccfea58f1afe65d0ad4e5d32db36dbba54f2
ilkin-um <ilkinum@gmail.com>
2022-07-04 12:18:59 +0400

    created job update functionality

--- backend/apis/v1/route_jobs.py
+++ backend/apis/v1/route_jobs.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException, status
 from schemas.jobs import CreateJob, ShowJob
 from db.session import get_db
-from db.repository.jobs import create_new_job, retreive_job, list_jobs
+from db.repository.jobs import create_new_job, retreive_job, list_jobs, update_job_by_id
 from typing import List
 
 router = APIRouter()
@@ -31,3 +31,14 @@ def read_job(id: int, db: Session = Depends(get_db)):
 def read_jobs(db: Session = Depends(get_db)):
     jobs = list_jobs(db=db)
     return jobs
+
+
+@router.put("/update/{id}")
+def update_job(id: int, job: CreateJob, db: Session = Depends(get_db)):
+    current_user = 1
+    message = update_job_by_id(id=id, job=job, db=db, owner_id=current_user)
+    if not message:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with id {id} not found"
+        )
+    return {"msg": "Successfully updated data."}
