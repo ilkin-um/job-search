d1b8cd290f2c98d9910cc33e9bf323ae12c82dc4
ilkin-um <ilkinum@gmail.com>
2022-07-08 15:38:22 +0400

    removed general router

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -4,7 +4,7 @@ from apis.v1 import route_general_pages, route_users, route_jobs, route_login
 
 api_router = APIRouter()
 
-api_router.include_router(route_general_pages.router, prefix="", tags=["general_pages"])
+# api_router.include_router(route_general_pages.router, prefix="", tags=["general_pages"])
 
 api_router.include_router(route_users.router, prefix="/users", tags=["users"])
 
